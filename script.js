70c5a809defd2167feb30b9741ec998308b3254e
crvest <chad.vest@gmail.com>
2022-03-29 22:02:17 -0400

    dojoweather complete

---
+++ script.js
@@ -0,0 +1,9 @@
+// on mouseclick in navbar send alert
+function cityAlert() {
+    alert("Loading weather report...");
+}
+
+// on mouse click, remove cookie window
+function removeCookie(element) {
+    element.remove();
+}
